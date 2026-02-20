7e51c752dcf7c1cfef7447ffd20a545148995f56
Vanz-007 <vv2902@srmist.edu.in>
2026-02-20 11:28:11 +0530

    Added the age code!

---
+++ Age.java
@@ -0,0 +1,8 @@
+public class Age{
+    public static void main(String[]args){
+        int birth=2000;
+        int current=2024;
+        int age=current-birth;
+        System.out.println("His age is"+age);
+    }
+}
