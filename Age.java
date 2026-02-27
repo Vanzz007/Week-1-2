25b37e1ade21bbcab5f13205c50293021c415220
Vanz-007 <vv2902@srmist.edu.in>
2026-02-27 10:54:28 +0530

    Level1 Question 1 solution

--- Age.java
+++ Age.java
@@ -1,8 +1,11 @@
-public class Age{
-    public static void main(String[]args){
-        int birth=2000;
-        int current=2024;
-        int age=current-birth;
-        System.out.println("His age is"+age);
-    }
+public class Age {
+   public Age() {
+   }
+
+   public static void main(String[] var0) {
+      short var1 = 2000;
+      short var2 = 2024;
+      int var3 = var2 - var1;
+      System.out.println("His age is" + var3);
+   }
 }
